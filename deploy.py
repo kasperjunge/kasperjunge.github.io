9b908917770589a626e4339ebac571e8735ea540
Kasper Junge <kasperjuunge@gmail.com>
2025-02-15 10:02:36 +0100

    update blog

--- deploy.py
+++ deploy.py
@@ -1,24 +1,14 @@
 import subprocess
 
-
 def deploy():
 
-    subprocess.run(["uv", "run", "ablog", "build"])
-
-    subprocess.run(["git", "add", "."])
-
-    subprocess.run(["git", "commit", "-m", "update blog"])
-
-    subprocess.run(
-        [
-            "uv", 
-            "run", 
-            "ablog", 
-            "deploy", 
-            "--github-branch", "main", 
-            "-w", "docs", 
-            "-g", "kasperjunge", 
-            "-p", "docs"])
+    subprocess.run("uv run ablog build", shell=True)
+    
+    subprocess.run("git add .", shell=True)
+    
+    subprocess.run('git commit -m "update blog"', shell=True)
+    
+    subprocess.run("uv run ablog deploy --github-branch main -w docs -g kasperjunge -p docs", shell=True)
 
 if __name__ == "__main__":
     deploy()
